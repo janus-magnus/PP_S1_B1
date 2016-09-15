d2c616683595f7ead70691fb38cc5df75dd96f8d
janus-magnus <Ykorringa@gmail.com>
2016-09-15 09:10:12 +0200

    o

--- Les4/FA4.py
+++ Les4/FA4.py
@@ -2,8 +2,6 @@ anulFile = open('anul.txt','r')
 ritenFile = open('riten.txt','r')
 herzienFile = open('herzienRit.txt','w')
 
-
-
 def herzien():
     anulList = []
     for line in anulFile:
@@ -18,9 +16,5 @@ def herzien():
         for anul in anulList:
             if anul not in rit:
                 herzienFile.write(str(rit)+'\n')
-
-
-#print(anulList())
-#print(ritList())
 
 herzien()
