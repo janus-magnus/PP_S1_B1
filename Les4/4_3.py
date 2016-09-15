f569a9aaf4cae17b259a6e8e98fbac586a55e998
janus-magnus <Ykorringa@gmail.com>
2016-09-15 09:53:09 +0200

    rest les 4

--- Les4/4_3.py
+++ Les4/4_3.py
@@ -4,10 +4,20 @@ file = open('Kaartnummers.txt', 'r')
 
 
 def fileReader():
+    lineList = []
     lineCounter = 0
     for line in file:
         lineCounter+=1
+        quickList = line.split(sep=', ')
+        lineList.append(quickList[0])
     print('Deze file telt ' + str(lineCounter) + ' regels')
+
+
+    maxNum = max(lineList)
+    maxLine = lineList.index(max(lineList))+1
+    print('het grootste kaartnummer is: '+str(maxNum)+' en dat staat op regel '+
+    str(maxLine))
+    file.close()
 
 
 fileReader()
--- Les4/4_4.py
+++ Les4/4_4.py
@@ -1,1 +1,20 @@
-file = open('hardlopers.txt', 'w')
+from pathlib import Path
+import time
+file = Path('hardlopers.txt')
+
+
+if file.is_file():
+    hardloopFile = open('hardlopers.txt', 'w')
+    #print('1')
+else:
+    hardloopFile = open('hardlopers.txt', 'a')
+    #print('2')
+
+dateStr = time.strftime('%a %d %b %Y,')
+runTimeStr = input("voer de gelopen tijd in: ")
+nameStr = input("voer de naam in: ")
+
+writeStr = dateStr + runTimeStr + ', '+ nameStr
+
+hardloopFile.write(writeStr)
+hardloopFile.close()
