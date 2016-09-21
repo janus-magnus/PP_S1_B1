e879b5a68e5070ef35e7ad3ad7656ac9b351e78c
janus-magnus <Ykorringa@gmail.com>
2016-09-21 20:18:03 +0200

    small change

--- Les7/7_3.py
+++ Les7/7_3.py
@@ -5,20 +5,23 @@ with open('gamers.csv', 'r') as gamerFile:
     gfreader = csv.reader(gamerFile)
 
     for line in gfreader:
-        scoreList = []
-        scoreList.append(line)
-        print(type(line))
-
+        gamerList = []
+        gamerList.append(line[0].split(sep=';'))
+ #       print(line[0].split(sep=';'))
+        print(len(gamerList))
 
     highscore = 0
 
+    for g in gamerList:
+        print(highscore)
+       # if highscore < eval(g[2]):
+        #    highscore = eval(g[2])
 
-    for g in scoreList:
-        print('ss')
-        if highscore < eval(g[2]):
-            highscore = eval(g[2])
+ #   print(highscore)
 
-    for g in scoreList:
+    for g in gamerList:
+  #      print(len(gamerList))
+  #      print(g[2])
         if highscore == g[2]:
             print('De hoogste score is: {} op {} behaald door {}'.format(g[2],g[0].g[1]))
 
