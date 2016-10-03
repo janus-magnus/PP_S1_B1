d9cea627a5aaf26b844bfc147dd2ed4cbda6153e
janus-magnus <ykorringa@gmail.com>
2016-10-03 13:16:14 +0200

    FA8

--- Les8/FA8.py
+++ Les8/FA8.py
@@ -1,52 +1,33 @@
-from xml.etree import ElementTree
 import xmltodict
 
-stations = ElementTree.parse('Stations.xml')
+with open('Stations.xml','r') as stationXML:
+    stationContent = stationXML.read()
+    stationDict = xmltodict.parse(stationContent)
+
+stations =  stationDict['Stations']['Station']
 
 
-
-code = stations.findall('Station/Code')
-type = stations.findall('Station/Type')
-naamL = stations.findall('Station/Namen/Lang')
-naamM = stations.findall('Station/Namen/Middel')
-naamK = stations.findall('Station/Namen/Kort')
-land = stations.findall('Station/Land')
-syno = stations.findall('Station/Synoniemen/Synoniem')
-
-print(len(syno))
-
-'''
 print('Dit zijn de codes, types en landcodes van de 4 stations:')
-for c, t, l in zip(code,type,land):
-    s = '{} - {} - {}'.format(c.text,t.text,l.text)
+for stat in stations:
+    s = '{} - {} - {}'.format(stat['Code'],stat['Type'],stat['Land'])
     print(s)
-'''
 
-
-testStr = ''
 print('\nDit zijn alle stations met een of meer synoniemen:')
-for c in code:
-    print(c.text, end=' - ')
-    tst = ''
-    for synoniem in syno:
-        if synoniem is not None:
-            tst = tst + synoniem.text + ' '
-            print(tst)
+for stat in stations:
+    if stat['Synoniemen'] is not None:
+        print('{} - {}'.format(stat['Code'], stat['Synoniemen']['Synoniem']))
 
-
-'''
 print('\nDit is de lange naam van elk station:')
-for c, n in zip(code,naamL):
-    s = '{} - {}'.format(c.text,n.text)
+for stat in stations:
+    s = '{} - {}'.format(stat['Code'],stat['Namen']['Lang'])
     print(s)
 
 print('\nDit is de middellange naam van elk station:')
-for c, n in zip(code,naamM):
-    s = '{} - {}'.format(c.text,n.text)
+for stat in stations:
+    s = '{} - {}'.format(stat['Code'],stat['Namen']['Middel'])
     print(s)
 
 print('\nDit is de korte naam van elk station:')
-for c, n in zip(code,naamK):
-    s = '{} - {}'.format(c.text,n.text)
+for stat in stations:
+    s = '{} - {}'.format(stat['Code'],stat['Namen']['Kort'])
     print(s)
-'''
